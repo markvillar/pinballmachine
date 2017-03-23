09e8cf12b5e63c88e1f7373fbc7f7cd0454f9161
Mark Villar <markcvillar@gmail.com>
2017-03-23 20:15:31 +0000

    Add another Demo

--- src/Demo.java
+++ src/Demo.java
@@ -37,7 +37,8 @@ public class Demo
         BallObject bumper2 = new BallObject(380, 420, Color.LIGHT_GRAY, 15, machine, bumperObjects);
         BallObject largeHole = new BallObject(470, 230, Color.BLACK, 45, machine, holeObjects);
         BallObject smallHole = new BallObject(60, 60, Color.BLACK, 15, machine, holeObjects);
-        FlashPinball flashPinball = new FlashPinball(200, 200, -7, 8, Color.ORANGE, 30, machine, bumperObjects, holeObjects, pinballObjects);
+        FlashPinball flashPinball1 = new FlashPinball(200, 200, -7, 8, Color.ORANGE, 30, machine, bumperObjects, holeObjects, pinballObjects);
+        FlashPinball flashPinball2 = new FlashPinball(400, 350, 7, 8, Color.GREEN, 30, machine, bumperObjects, holeObjects, pinballObjects);
         PinballObject pinballObj1 = new PinballObject(50, 200, -5, 3, Color.RED, 10, machine, pinballObjects);
         PinballObject pinballObj2 = new PinballObject(100, 300, 1, 2, Color.BLUE, 55, machine, pinballObjects);
         PinballObject pinballObj3 = new PinballObject(450, 125, -1, -1, Color.YELLOW, 40, machine, pinballObjects);
@@ -54,8 +55,10 @@ public class Demo
             pinballObj3.collisionCheck(bumperObjects, holeObjects, pinballObjects);
             pinballObj4.move();
             pinballObj4.collisionCheck(bumperObjects, holeObjects, pinballObjects);
-            flashPinball.move();
-            flashPinball.collisionCheck(bumperObjects, holeObjects, pinballObjects);
+            flashPinball1.move();
+            flashPinball1.collisionCheck(bumperObjects, holeObjects, pinballObjects);
+            flashPinball2.move();
+            flashPinball2.collisionCheck(bumperObjects, holeObjects, pinballObjects);
         }
     }
 }
